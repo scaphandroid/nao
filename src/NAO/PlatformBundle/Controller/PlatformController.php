1045f887b9ab254e5425ed3a0cfb936d0a6f61dc
Phil <philippine.chombart@gmail.com>
2016-11-14 15:33:36 +0100

    routing home, recherche & observer&

--- src/NAO/PlatformBundle/Controller/PlatformController.php
+++ src/NAO/PlatformBundle/Controller/PlatformController.php
@@ -3,11 +3,30 @@
 namespace NAO\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class PlatformController extends Controller
 {
     public function indexAction()
     {
         return $this->render('NAOPlatformBundle:Platform:index.html.twig');
+    }
+
+    public function rechercherAction($id)
+    {
+        if ($id == null) {// afficher toutes les observations
+            return new Response("Affichage de toutes les observations");
+        }
+        else {
+            return $this->render('NAOPlatformBundle:Platform:rechercher.html.twig', array(
+                'especeId'=>$id
+            ));
+
+        }
+    }
+
+    public function observerAction()
+    {
+        return $this->render('NAOPlatformBundle:Platform:observer.html.twig');
     }
 }
