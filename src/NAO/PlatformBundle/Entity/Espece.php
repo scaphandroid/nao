1007e6ee75dc7baf915e58cc81115ab0b0f9ace2
Phil <philippine.chombart@gmail.com>
2016-11-14 13:51:30 +0100

    Mise en place des entités

---
+++ src/NAO/PlatformBundle/Entity/Espece.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace NAO\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Espece
+ *
+ * @ORM\Table(name="espece")
+ * @ORM\Entity(repositoryClass="NAO\PlatformBundle\Repository\EspeceRepository")
+ */
+class Espece
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_vern", type="string", length=255)
+     */
+    private $nomVern;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Espece
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     *
+     * @return Espece
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * Set nomVern
+     *
+     * @param string $nomVern
+     *
+     * @return Espece
+     */
+    public function setNomVern($nomVern)
+    {
+        $this->nomVern = $nomVern;
+
+        return $this;
+    }
+
+    /**
+     * Get nomVern
+     *
+     * @return string
+     */
+    public function getNomVern()
+    {
+        return $this->nomVern;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Espece
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
+
