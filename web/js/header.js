ed1c95378f350e90633cbacc7dc08f6d80756b0b
scaphandroid <arl-informatique@gmx.ch>
2016-11-21 09:11:26 +0100

    header fixe à partir de 110px avec logo et recherche qui glissent à côté du menu de navigation

---
+++ web/js/header.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var fixed = false ;
+    var moved = false;
+    $(document).scroll(function(){
+        if($(window).scrollTop() > 60 && !moved){
+            $('#barre-recherche').animate({
+                top: "90px",
+                left: "100px"
+                }
+            );
+            $('#logo-header').animate({
+                    top: "90px"
+                }
+            );
+            moved = true;
+        }
+        if($(window).scrollTop() < 60 && moved){
+            $('#barre-recherche').animate({
+                    top: "0px",
+                    left: "0px"
+                }
+            );
+            $('#logo-header').animate({
+                    top: "0px"
+                }
+            );
+            moved = false;
+        }
+        if($(window).scrollTop() > 110 && !fixed){
+            $('header').addClass("header-fixed");
+            $('.body').addClass("body-margin");
+            fixed = true;
+        }
+        if($(window).scrollTop() < 110 && fixed){
+            $('header').removeClass("header-fixed");
+            $('.body').removeClass("body-margin");
+            fixed = false;
+        }
+    })
+});
+
+
