7bbfe2aa6d55b4c9ba072312ace03e1023eda9cf
Phil <philippine.chombart@gmail.com>
2016-11-21 15:03:40 +0100

    1ere mise en place de FOSUserBundle

--- src/NAO/PlatformBundle/NAOPlatformBundle.php
+++ src/NAO/PlatformBundle/NAOPlatformBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NAOPlatformBundle extends Bundle
 {
+      public function getParent()
+      {
+          return 'FOSUserBundle';
+      }
 }
